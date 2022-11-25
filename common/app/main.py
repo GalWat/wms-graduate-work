5463bee65e64b8f723cc7871ce2b70c25d3bd859
GalWat <watts-galeo@ya.ru>
2022-11-25 23:09:05 +0300

    Add FastAPI draft

--- common/app/main.py
+++ common/app/main.py
@@ -1,24 +1,32 @@
 import psycopg2
 from config import settings
+from fastapi import FastAPI
+from models import LocationGroup
+
+app = FastAPI()
+
+conn = psycopg2.connect(
+    host=settings.db.host,
+    database=settings.db.database,
+    user=settings.db.user,
+    password=settings.db.password,
+    port=settings.db.port
+)
+cur = conn.cursor()
 
 
-def main():
-    conn = psycopg2.connect(
-        host=settings.db.host,
-        database=settings.db.database,
-        user=settings.db.user,
-        password=settings.db.password,
-        port=settings.db.port
-    )
-    cur = conn.cursor()
-    
-    cur.execute("SELECT * FROM weather")
-    rows = cur.fetchall()
-    print(rows)
-    
-    cur.close()
-    conn.close()
+@app.get("/")
+async def read_root():
+    return {
+        "Hello": "World"
+    }
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/location_groups/create")
+async def create_location_group(location_group: LocationGroup) -> int:
+    """Create a location group"""
+    cur.execute(f"INSERT INTO location_groups VALUES (DEFAULT, '{location_group.name}') RETURNING id")
+    conn.commit()
+
+    row = cur.fetchone()
+    return row[0]
