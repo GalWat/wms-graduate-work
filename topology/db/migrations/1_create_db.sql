6e51952474b76c09b6c26855789b59115b388c1a
GalWat <watts-galeo@ya.ru>
2022-11-30 14:08:12 +0300

    Delete topology

--- topology/db/migrations/1_create_db.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-CREATE TABLE weather (
-    city            varchar(80),
-    temp_lo         int,           -- low temperature
-    temp_hi         int,           -- high temperature
-    prcp            real,          -- precipitation
-    date            date
-);
--- migrate:down
